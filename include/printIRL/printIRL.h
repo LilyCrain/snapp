4b6e2a5fa13f815c70d3bb1a368bb9610f684619
LilyCrain <lilycrain05@gmail.com>
2023-05-14 22:12:08 -0500

    Added printIRL, instantiatePrintIRL, and lastLine to rootIO.cpp Added printIRL.h Added printing.snapp
    Create branch Windows-IDE-Only
    Changes to be committed:
        new file:   include/printIRL/printIRL.h
    	new file:   samples/printing.snapp
    	modified:   src/native/root/rootIO.cpp

---
+++ include/printIRL/printIRL.h
@@ -0,0 +1,14 @@
+#ifndef SNAPP_PRINTIRL_PRINTIRL_H_
+#define SNAPP_PRINTIRL_PRINTIRL_H_
+
+#include <string>
+
+namespace Snapp::PrintIRL {
+
+    void instantiatePrintIRL();
+    void lastLine();
+    void printIRL(std::string text);
+
+}
+
+#endif
--- src/native/root/rootIO.cpp
+++ src/native/root/rootIO.cpp
@@ -2,6 +2,8 @@
 #include "value/dataType.h"
 #include "semantics/astRunner.h"
 #include "value/import.h"
+#include <fstream>
+#include <windows.h>
 
 namespace Snapp::Native {
 
@@ -58,13 +60,68 @@ namespace Snapp::Native {
         return inputFunction;
     }
 
+    FunctionValue instantiatePrintIRL(){
+        FunctionValue instantiatePrintIRL;
+        instantiatePrintIRL.addOverload({
+            DataType::Void,
+            {},
+            [](const std::vector<DataValue>& args, const Scope* scope) {
+                std::ofstream tempfile("temp.txt"); 
+                tempfile.close();
+                return std::nullopt;
+            }
+        });
+        return instantiatePrintIRL;
+    }
+
+    FunctionValue printIRL(){
+        FunctionValue printIRL;
+        printIRL.addOverload({
+            DataType::Void,
+            {DataType::Void},
+            [](const std::vector<DataValue>& args, const Scope* scope){
+                std::ofstream tempFile("tempPrint.txt", std::ios::app);
+                tempFile << *convertStr(args.at(0)) + '\n'; 
+                tempFile.close();
+                return std::nullopt;
+            }
+        });
+        return printIRL;
+    }
+
+    FunctionValue lastLine(){
+        FunctionValue lastLine;
+        lastLine.addOverload({
+            DataType::Void,
+            {},
+            [](const std::vector<DataValue>& args, const Scope* scope){
+                // print temp file to default printer
+                const char* printername = NULL;
+                STARTUPINFO startup_info = {0};
+                startup_info.cb = sizeof(STARTUPINFO);
+                PROCESS_INFORMATION process_info;
+                DWORD exit_code;
+    
+                // use ShellExecute to print file
+                ShellExecute(NULL, "print", "tempPrint.txt", NULL, NULL, SW_HIDE);
+                return std::nullopt;
+            }
+        });
+        return lastLine;
+    }
+
+
+
+
     ImportValue* createRootIO() {
         auto scope = new Scope(nullptr, true);
         auto rootIO = new ImportValue(scope);
 
         scope->add("print", createPrintFunction());
         scope->add("input", createInputFunction());
-
+        scope->add("instantiatePrintIRL", instantiatePrintIRL());
+        scope->add("printIRL", printIRL());
+        scope->add("lastLine", lastLine());
         return rootIO;
     }
 
